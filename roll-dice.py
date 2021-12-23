efd75653322760ab7ea5981df4b4e0f447fbc31c
dannesx <daniel.viantunes@gmail.com>
2021-12-23 16:17:27 -0300

    Correção em roll-dice.py

--- roll-dice.py
+++ roll-dice.py
@@ -5,8 +5,7 @@ def rollDice(size):
 
 size = int(input("Digite o Nº de lados do dado: "))
 
-if size < 2:
-  while size < 2:
-    size = int(input(f"Não existem dados de {size} lado! Por favor, tente novamente!: "))
+while size < 2:
+  size = int(input(f"Não existem dados de {size} lado! Por favor, tente novamente!: "))
 
 print(f"D[{size}] → {rollDice(size)}")
